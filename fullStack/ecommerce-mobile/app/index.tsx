78c0368048c169c0f8f6c358e400700e7d4045cf
maispower <167310158+maispower@users.noreply.github.com>
2025-01-17 07:02:44 +0100

    install GlueStack

--- fullStack/ecommerce-mobile/app/index.tsx
+++ fullStack/ecommerce-mobile/app/index.tsx
@@ -1,7 +1,7 @@
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 import products from '../assets/products.json';
 import { ProductListItem } from '../components/ProductListItem';
-
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 export default function HomeScreen() {
   return (
    <FlatList
